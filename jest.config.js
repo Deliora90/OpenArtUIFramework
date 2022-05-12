8dd20a4573a32ded1d54b6e980c280403161cd2f
Sakharova Mariya <deliora90@gmail.com>
2022-05-12 22:09:46 +0300

    Fixed the configs for testing

--- jest.config.js
+++ jest.config.js
@@ -12,5 +12,9 @@ module.exports = {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'identity-obj-proxy',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '@utils/(.*)': '<rootDir>/src/utils/$1',
+    '@styles/(.*)': '<rootDir>/src/styles/$1',
+    '@components/(.*)': '<rootDir>/src/components/$1',
+    '@icons/(.*)': '<rootDir>/src/icons/$1',
   },
 };
